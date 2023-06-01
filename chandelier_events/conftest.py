8df569646a219e0ab18614d2eab32fcef4866854
Jesus <alexot855@gmail.com>
2023-06-01 11:23:35 -0600

    Chandelier App | nueva version, cambios en la estructura del proyecto y en el codigo del mismo.

---
+++ chandelier_events/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from chandelier_events.users.models import User
+from chandelier_events.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
