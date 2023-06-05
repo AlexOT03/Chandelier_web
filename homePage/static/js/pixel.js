f8c04ebb8d76815d0c678764b68fac4af2a3c49d
Jesus <alexot855@gmail.com>
2023-06-05 13:59:42 -0600

    Chandelier App | Aplicacion en desarrollo, ligeros retoques en el apartado INDEX y en el apartado PLACES

--- homePage/static/js/pixel.js
+++ homePage/static/js/pixel.js
@@ -123,3 +123,21 @@ d.addEventListener("DOMContentLoaded", function(event) {
     }
 
 });
+
+// Get the button
+var backToTopButton = document.getElementById("back-to-top");
+      
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopButton.onclick = function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
--- homePage/urls.py
+++ homePage/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.indexHome.as_view(), name='indexHome'),
     path('fast-quote/', views.fastQuoteHome.as_view(), name='fastQuoteHome'),
-    path('location/<id>/', views.locationHome.as_view(), name='locationHome'),
-    path('location/info/<id>/', views.locationInfoHome.as_view(), name='locationInfoHome'),
+    path('location/<int:id>/<str:name>', views.locationHome.as_view(), name='locationHome'),
+    path('location/info/<int:id>/', views.locationInfoHome.as_view(), name='locationInfoHome'),
     path('quote/<id>', views.quoteHome.as_view(), name='quoteHome'),
     path('about-us/', views.aboutUsHome.as_view(), name='aboutHome'),
     path('contact-us/', views.contactHome.as_view(), name='contactHome'),
